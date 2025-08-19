50b4008ed47fa8c74373541f6aaabcbbbbf69b98
UdayashankarAradhya <udayinventive@gmail.com>
2025-08-19 19:24:58 +0530

    Add a program to implement singly linked list and perform operations on it

---
+++ practice/LinkedLists/singly_linked_list.cpp
@@ -0,0 +1,129 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+    Node(int data){
+       this->data=data;
+       this->next=nullptr;
+    }
+};
+Node *insertAtHead(Node *head, int x){
+    Node *temp=new Node(x);
+    temp->next=head;
+    head=temp;
+    return head;
+}
+Node *insertAtTail(Node *head, int x){
+    Node *temp=new Node(x);
+    if(head==NULL) return temp;
+    Node *curr=head;
+    while(curr->next!=NULL){
+        curr=curr->next;
+    }
+    curr->next=temp;
+    temp->next=NULL;
+    return head;
+}
+int search(Node *head, int x){
+    int pos=1;
+    Node *curr=head;
+    while(curr!=NULL){
+        if(curr->data==x){
+            return pos;
+        }
+        else{
+            pos++;
+            curr=curr->next;
+        }
+    }
+    return -1;
+}
+void print_list(Node *head){
+    Node *curr=head;
+    while(curr!=NULL){
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }
+
+}
+Node *takeinput(){
+    int data;
+    cin>>data;
+    Node *head=NULL;
+    Node *prev=NULL;
+    while(data!=-1){
+        Node *input=new Node(data);
+        if(head==NULL){
+            head=input;
+            prev=input;
+        }
+        else{
+            prev->next=input;
+            prev=input;
+        }
+        cin>>data;
+    }
+    return head;
+
+}
+int main(){
+    Node* head = nullptr; 
+    int choice;
+    int data, position;
+
+    do {
+        cout << "\n--- Linked List Menu ---" << endl;
+        cout << "1. Create a new list" << endl;
+        cout << "2. Print the list" << endl;
+        cout << "3. Insert at Head" << endl;
+        cout << "4. Insert at Tail" << endl;
+        cout << "5. Search for an element" << endl;
+        cout << "0. Exit" << endl;
+        cout << "Choose an option: ";
+        cin >> choice;
+
+        switch(choice){
+            case 1:
+                head = takeinput();
+                break;
+            case 2:
+                cout << "Current list: ";
+                print_list(head);
+                break;
+            case 3:
+                cout << "Enter data to insert at head: ";
+                cin >> data;
+                head = insertAtHead(head, data);
+                cout << data << " inserted at the head." << endl;
+                break;
+            case 4:
+                cout << "Enter data to insert at tail: ";
+                cin >> data;
+                head = insertAtTail(head, data);
+                cout << data << " inserted at the tail." << endl;
+                break;
+            case 5:
+                cout << "Enter element to search for: ";
+                cin >> data;
+                position = search(head, data);
+                if (position != -1) {
+                    cout << "Element " << data << " found at position " << position << "." << endl;
+                } else {
+                    cout << "Element " << data << " not found in the list." << endl;
+                }
+                break;
+            case 0:
+                cout << "Exiting program." << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    } while (choice != 0);
+
+    return 0;
+}
+
+
